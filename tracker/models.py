f63e03a489336c21b149918c4a156b8bdc8eb25a
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-02-22 04:24:39 +0530

    Added form to DB connections

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class TrackingHistory(models.Model):
+    current_balance = models.ForeignKey('CurrentBalance', on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    expense_type = models.CharField(max_length=50, choices=[
+        ('Credit', 'Credit'),
+        ('Debit', 'Debit')
+    ])
+    amt = models.IntegerField()
+    category = models.CharField(max_length=50, choices=[
+        ('Food', 'Food'),
+        ('Transport', 'Transport'),
+        ('Utilities', 'Utilities'),
+        ('Rent', 'Rent'),
+        ('Entertainment', 'Entertainment'),
+        ('Miscellaneous', 'Miscellaneous')
+    ])
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CurrentBalance(models.Model):
+    cur_bal = models.IntegerField(default=0)
--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from .models import TrackingHistory, CurrentBalance
 
 # Create your views here.
 def index(r):
+    if r.method == 'POST':
+        title = r.POST.get('title')
+        amount = r.POST.get('amount')
+        expense_type = 'Credit'
+        category = r.POST.get('category')
+        date_time = r.POST.get('date_time') or timezone.now()
+        current_bal, _ = CurrentBalance.objects.get_or_create(id = 1)
+        if int(amount) < 0: expense_type = 'Debit'
+        current_bal.cur_bal += int(amount)
+        current_bal.save()
+        TrackingHistory.objects.create(
+            current_balance = current_bal,
+            title=title,
+            amt=amount,
+            expense_type = expense_type,
+            category=category,
+            created_at=date_time
+        )
+        return redirect('/')
     return render(r, 'index.html')
